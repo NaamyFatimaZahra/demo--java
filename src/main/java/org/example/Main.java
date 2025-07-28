0db1c64eac2c3230869853a048bfc98984748699
naamyfatimazahra <naamyfatimazahra@gmail.com>
2025-07-28 04:59:56 +0100

    solve build file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,11 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
+
+        String port = System.getenv("PORT");
+        if (port != null) {
+            port(Integer.parseInt(port));
+        }
         get("/", (req, res) -> "Hello World!");
     }
 }
